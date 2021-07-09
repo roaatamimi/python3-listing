f815032925df08929491620c023bd37dc3823f68
roaatamimi <78821923+roaatamimi@users.noreply.github.com>
2021-07-09 23:55:58 +0300

    Add files via upload

---
+++ roaa.py
@@ -0,0 +1,16 @@
+fileName = './od.txt'
+with open(fileName) as f:
+    lines = f.readlines()
+
+def divide_chunks(l, n):
+    for i in range(0, len(l), n): 
+        yield l[i:i + n]
+  
+groupSize = 4
+groups = list(divide_chunks(lines, groupSize))
+for i in groups:
+	print(i)
+	print("\n")
+	
+	
+
